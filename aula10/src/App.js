8b50312baecce8ecb7955ba8df8ae06eb8dfb9a7
techcomkathia <techcomk@gmail.com>
2023-12-10 19:55:54 -0300

    lista de filmes por gênero

--- aula10/src/App.js
+++ aula10/src/App.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react';
-import './App.css';
 
 function App() {
-  // Estado para armazenar os gêneros
   const [generos, setGeneros] = useState([]);
+  const [filmesPorGenero, setFilmesPorGenero] = useState([]);
+  const [generoSelecionado, setGeneroSelecionado] = useState(null);
 
-  // Função para buscar os gêneros da API
   async function buscarGeneros() {
     try {
       const apiKey = 'b4b5f9d98442f11bbdd50a5adf70f1d1';
@@ -20,24 +19,65 @@ function App() {
 
       const dados = await resposta.json();
       setGeneros(dados.genres);
-      console.log(generos)
     } catch (error) {
-      console.log('Ocorreu um erro ao carregar os gêneros:', error.message);
+      console.error('Ocorreu um erro ao carregar os gêneros:', error.message);
     }
   }
 
-  // Efeito para buscar os gêneros ao montar o componente
-  useEffect(() => { buscarGeneros() }, []);
+  async function buscarFilmesPorGenero(genero) {
+    try {
+      const apiKey = 'b4b5f9d98442f11bbdd50a5adf70f1d1';
+      const linguagem = 'pt-BR';
+      const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=${linguagem}&with_genres=${genero.id}`;
 
-  // Renderização dos gêneros
+      const resposta = await fetch(url);
+
+      if (!resposta.ok) {
+        throw new Error('Erro ao buscar os filmes por gênero');
+      }
+
+      const dados = await resposta.json();
+      setFilmesPorGenero(dados.results);
+    } catch (error) {
+      console.error('Ocorreu um erro ao carregar os filmes por gênero:', error.message);
+    }
+  }
+
+  useEffect(() => {
+    buscarGeneros();
+  }, []);
+
+  useEffect(() => {
+    if (generoSelecionado !== null) {
+      buscarFilmesPorGenero(generoSelecionado);
+      console.log(filmesPorGenero)
+    }
+  }, [generoSelecionado]);
+
   return (
     <div className="App">
       <h1>Gêneros:</h1>
       <ul>
         {generos.map(genero => (
-          <li style={{listStyle:'none'}} key={genero.id}><button>{genero.name}</button></li>
+          <li style={{ listStyle: 'none' }} key={genero.id}>
+            <button onClick={() => setGeneroSelecionado(genero)}>{genero.name}</button>
+          </li>
         ))}
       </ul>
+
+      {generoSelecionado !== null && (
+        <div>
+          <h2>Filmes de {generoSelecionado.name}:</h2>
+          <ul>
+            {filmesPorGenero.map(filme => (
+              <li key={filme.id}>
+                <h2>{filme.title}</h2>
+                
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
